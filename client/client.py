2506e065db09f85796890b30200a0e63a95f5ca2
PuneethYenneti06 <puneeth9506@gmail.com>
2026-03-16 23:18:31 +0530

    player response logic added to client code

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 import socket
 import ssl
+import threading
 from pathlib import Path
 
 HOST = "10.5.25.223"
@@ -9,9 +10,22 @@ SECURITY_DIR = Path(__file__).resolve().parents[1] / "security"
 CA_FILE = SECURITY_DIR / "certs" / "ca.crt"
 
 
+def receive_messages(sock):
+    while True:
+        try:
+            message = sock.recv(1024).decode()
+            if not message:
+                break
+            print(message)
+        except:
+            print("Disconnected from server.")
+            break
+
+
 def start_client():
     raw_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Certificate Authority (CA)
+
+    # TLS setup
     context = ssl.create_default_context()
     context.check_hostname = False
     context.verify_mode = ssl.CERT_NONE
@@ -21,16 +35,20 @@ def start_client():
 
     print(f"Connected with TLS cipher: {client.cipher()}")
 
-    message = client.recv(1024)
-    print(message.decode())
+    # start background thread to receive quiz messages
+    thread = threading.Thread(target=receive_messages, args=(client,), daemon=True)
+    thread.start()
 
     while True:
-        msg = input("Enter message (type 'quit' to exit): ")
-        if msg.lower() == "quit":
+        answer = input()
+
+        if answer.lower() == "quit":
             break
-        client.send(msg.encode())
+
+        client.send(answer.encode())
 
     client.close()
 
 
-start_client()
+if __name__ == "__main__":
+    start_client()
